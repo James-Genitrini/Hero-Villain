ada2e5d9d26688b04cf1c18d6ade7697f5050db5
James Genitrini <jamesgenitrini@gmail.com>
2025-03-20 10:07:54 +0100

    Store

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,86 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-  },
-  getters: {
+    organizationPassword: '',
+    heroAliases: [],
+    selectedHero: null,
+    teams: [],
+    selectedTeam: null,
+    organizationNames: [],
+    selectedOrganization: null,
   },
   mutations: {
+    setOrganizationPassword(state, password) {
+      state.organizationPassword = password;
+    },
+    setHeroAliases(state, aliases) {
+      state.heroAliases = aliases;
+    },
+    setSelectedHero(state, hero) {
+      state.selectedHero = hero;
+    },
+    updateHero(state, hero) {
+      if (state.selectedHero && state.selectedHero._id === hero._id) {
+        state.selectedHero = { ...state.selectedHero, ...hero };
+      }
+    },
+    setTeams(state, teams) {
+      state.teams = teams;
+    },
+    setSelectedTeam(state, team) {
+      state.selectedTeam = team;
+    },
+    updateTeam(state, team) {
+      if (state.selectedTeam && state.selectedTeam._id === team._id) {
+        state.selectedTeam = { ...state.selectedTeam, ...team };
+      }
+    },
+    setOrganizationNames(state, names) {
+      state.organizationNames = names;
+    },
+    setSelectedOrganization(state, org) {
+      state.selectedOrganization = org;
+    },
+    updateOrganization(state, org) {
+      if (state.selectedOrganization && state.selectedOrganization._id === org._id) {
+        state.selectedOrganization = { ...state.selectedOrganization, ...org };
+      }
+    },
   },
   actions: {
+    setOrganizationPassword({ commit }, password) {
+      commit('setOrganizationPassword', password);
+    },
+    setHeroAliases({ commit }, aliases) {
+      commit('setHeroAliases', aliases);
+    },
+    setSelectedHero({ commit }, hero) {
+      commit('setSelectedHero', hero);
+    },
+    updateHero({ commit }, hero) {
+      commit('updateHero', hero);
+    },
+    setTeams({ commit }, teams) {
+      commit('setTeams', teams);
+    },
+    setSelectedTeam({ commit }, team) {
+      commit('setSelectedTeam', team);
+    },
+    updateTeam({ commit }, team) {
+      commit('updateTeam', team);
+    },
+    setOrganizationNames({ commit }, names) {
+      commit('setOrganizationNames', names);
+    },
+    setSelectedOrganization({ commit }, org) {
+      commit('setSelectedOrganization', org);
+    },
+    updateOrganization({ commit }, org) {
+      commit('updateOrganization', org);
+    },
   },
-  modules: {
-  }
-})
+});
