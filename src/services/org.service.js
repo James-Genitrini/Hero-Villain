84aec0662df535d16bfccc3db4a8524783705227
James Genitrini <jamesgenitrini@gmail.com>
2025-04-05 21:05:22 +0200

    ajout try catch

--- src/services/org.service.js
+++ src/services/org.service.js
@@ -7,7 +7,7 @@ const getAllOrgs = async () => {
     return response.data; 
   } catch (error) {
     console.error("Erreur lors de la récupération des organisations :", error.message);
-    throw error;
+    throw error;  
   }
 };
 
@@ -18,38 +18,45 @@ const createOrg = async (data) => {
     return response.data;
   } catch (error) {
     console.error("Erreur lors de la création de l'organisation :", error.message);
-    throw error;
+    throw error; 
   }
 };
 
 const addTeamToOrg = async (orgSecret, teamId) => {
-      const response = await axiosService.patch(
-        `/orgs/addteam?org-secret=${orgSecret}`, 
-        { idTeam: teamId }
-      );
-      return response; 
-  };
-    
+  try {
+    const response = await axiosService.patch(
+      `/orgs/addteam?org-secret=${orgSecret}`, 
+      { idTeam: teamId }
+    );
+    return response; 
+  } catch (error) {
+    console.error("Erreur lors de l'ajout de l'équipe à l'organisation :", error.message);
+    throw error; 
+  }
+};
 
-  const removeTeamFromOrg = async (orgSecret, teamId) => {
-      const response = await axiosService.patch(
-        `/orgs/removeteam?org-secret=${orgSecret}`, 
-        { idTeam: teamId }
-      );
-      return response;
-  };
-  
+const removeTeamFromOrg = async (orgSecret, teamId) => {
+  try {
+    const response = await axiosService.patch(
+      `/orgs/removeteam?org-secret=${orgSecret}`, 
+      { idTeam: teamId }
+    );
+    return response;
+  } catch (error) {
+    console.error("Erreur lors de la suppression de l'équipe de l'organisation :", error.message);
+    throw error;  
+  }
+};
 
-  const getOrgById = async (orgId, secret) => {
-    try {
-      const response = await axiosService.get(`/orgs/getbyid/${orgId}?org-secret=${secret}`);
-      // console.log("Organisation récupérée :", response.data);
-      return response.data[0]; 
-    } catch (error) {
-      throw error.response?.data || error;
-    }
-  };
-  
+const getOrgById = async (orgId, secret) => {
+  try {
+    const response = await axiosService.get(`/orgs/getbyid/${orgId}?org-secret=${secret}`);
+    return response.data[0]; 
+  } catch (error) {
+    console.error("Erreur lors de la récupération de l'organisation :", error.message);
+    throw error.response?.data || error; 
+  }
+};
 
 export default {
   getAllOrgs,
