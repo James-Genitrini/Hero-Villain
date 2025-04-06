f5e878ed0add9dd4f68adeabad240bb4cf9a7da9
James Genitrini <jamesgenitrini@gmail.com>
2025-04-06 11:52:03 +0200

    routage

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-
-import Home from '@/components/Home.vue';
+import HomeView from '@/components/Home.vue';
 import TestHeroAliases from '@/components/TestHero.vue';
 import TestHeroCreate from '@/components/TestHeroCreate.vue';
 import TestHeroId from '@/components/TestHeroId.vue';
@@ -18,25 +17,33 @@ import TestOrgDelete from '@/components/TestOrgDelete.vue';
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '/', component: Home, name: 'Home' },
+  { path: '/', component: HomeView, name: 'Home' },
   { path: '/heroes', component: TestHeroAliases, name: 'TestHeroAliases' },
   { path: '/heroes/create', component: TestHeroCreate, name: 'TestHeroCreate' },
-  { path: '/heroes/:id', component: TestHeroId, name: 'TestHeroId' },
-  { path: '/heroes/update', component: TestHeroUpdate, name: 'TestHeroUpdate' },
+  { path: '/heroes/:id', component: TestHeroId, name: 'TestHeroId', meta: { requiresAuth: true } },
+  { path: '/heroes/update', component: TestHeroUpdate, name: 'TestHeroUpdate', meta: { requiresAuth: true } },
   { path: '/teams', component: TestTeam, name: 'TestTeam' },
   { path: '/teams/create', component: TestTeamCreate, name: 'TestTeamCreate' },
   { path: '/teams/manage', component: TestTeamManagement, name: 'TestTeamManagement' },
   { path: '/orgs', component: TestOrg, name: 'TestOrg' },
   { path: '/orgs/create', component: TestOrgCreate, name: 'TestOrgCreate' },
-  { path: '/orgs/:id', component: TestOrgId, name: 'TestOrgId' },
-  { path: '/orgs/addteam', component: TestOrgTeam, name: 'TestOrgTeam' },
-  { path: '/orgs/removeteam', component: TestOrgDelete, name: 'TestOrgDelete' },
-  { path: '*', redirect: '/' }
+  { path: '/orgs/:id', component: TestOrgId, name: 'TestOrgId', meta: { requiresAuth: true } },
+  { path: '/orgs/addteam', component: TestOrgTeam, name: 'TestOrgTeam', meta: { requiresAuth: true } },
+  { path: '/orgs/removeteam', component: TestOrgDelete, name: 'TestOrgDelete', meta: { requiresAuth: true } },
+  { path: '*', redirect: '/' },
 ];
 
 const router = new VueRouter({
   mode: 'history',
-  routes
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !localStorage.getItem('secretPhrase')) {
+    next('/secret');  
+  } else {
+    next();  
+  }
 });
 
 export default router;
